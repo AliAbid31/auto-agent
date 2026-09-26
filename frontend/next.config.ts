1a631cecb646a1a4e71a47c7676bda57c02e6bf4
AliAbid31 <oa_abid@esi.dz>
2026-09-26 12:52:25 +0100

    fix next.config for cloudflare standalone

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Génère un dossier HTML/CSS/JS statique ultra-rapide pour Cloudflare
+  output: 'standalone', // Génère un dossier HTML/CSS/JS statique ultra-rapide pour Cloudflare
   images: {
     unoptimized: true, // Requis si vous utilisez la balise <Image /> de Next.js
   },
